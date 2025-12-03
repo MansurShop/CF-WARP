9d2e320ef7e5657b56e515f13bffd4fc4233c5ba
MansurShop <158551282+MansurShop@users.noreply.github.com>
2025-12-04 07:28:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ TIME=$(date +'%Y-%m-%d %H:%M:%S')
 RAMMS=$(free -m | awk 'NR==2 {print $2}')
 KEY="2145515560:AAE9WqfxZzQC-FYF1VUprICGNomVfv6OdTU"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
-REPO="https://raw.githubusercontent.com/NevermoreSSH/cfwarp/main/"
+REPO="https://github.com/MansurShop/CF-WARP/main/"
 APT="apt-get -y install"
 start=$(date +%s)
 
